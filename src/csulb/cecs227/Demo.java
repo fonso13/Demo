41910a33717dfc1de9ac37442946ad176129bdf8
Alfonso <alfonso.villalobos@student.csulb.edu>
2020-02-04 16:42:10 -0800

    add .gitignore file ignoring all the setting files

--- src/csulb/cecs227/Demo.java
+++ src/csulb/cecs227/Demo.java
@@ -5,5 +5,8 @@ public class Demo {
     public static void main(String[] args){
         System.out.println("hello");
         System.out.println("I hope this worked!!!");
+
+        System.out.println("I don't think this worked :)");
+
     }
 }
